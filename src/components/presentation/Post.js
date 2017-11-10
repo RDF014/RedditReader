2aac0852efc8d50de4996e433f1a4aa748c109b3
RDF014 <rdf014@gmail.com>
2017-11-09 21:12:29 -0800

    fixed styles

--- src/components/presentation/Post.js
+++ src/components/presentation/Post.js
@@ -4,14 +4,20 @@ import defaultThumbnail from '../../thumbnail.png';
 const Post = (props) => {
   const {data} = props;
   const {
+    subreddit_name_prefixed,
     thumbnail,
     title,
     url
   } = data;
   return (
     <div className="Post">
-      <img className="Thumbnail" alt="thumbnail" src={thumbnail ? thumbnail : defaultThumbnail}/>
-      <a href={url}>{title}</a>
+      <figure>
+        <img className="Thumbnail" alt="thumbnail" src={thumbnail ? thumbnail : defaultThumbnail}/>
+        <figcaption>{subreddit_name_prefixed}</figcaption>
+      </figure>
+      <article>
+        <a href={url}>{title}</a>
+      </article>
     </div>
   )
 };
